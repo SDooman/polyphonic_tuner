42367b3d6866274786866b7cc0edbc0d4aa359f3
Nick Demarco <nicholas_demarco@brown.edu>
2015-10-26 15:34:15 -0400

    [ND] added example code to note_processing folder

---
+++ note_processing/chord_namer_2/masks.h
@@ -0,0 +1,40 @@
+//
+//  masks.h
+//  chord_namer_2
+//
+//  Created by Nick DeMarco on 8/16/15.
+//  Copyright (c) 2015 Nick DeMarco. All rights reserved.
+//
+
+#ifndef chord_namer_2_masks_h
+#define chord_namer_2_masks_h
+
+#define C 0
+#define Cs 1
+#define D 2
+#define Ds 3
+#define E 4
+#define F 5
+#define Fs 6
+#define G 7
+#define Gs 8
+#define A 9
+#define As 10
+#define B 11
+
+// BitMasks for 12 major triads
+
+#define CBIT 145
+#define CsBIT 290
+#define DBIT 580
+#define DsBIT 1160
+#define EBIT 2192
+#define FBIT 545
+#define FsBIT 1090
+#define GBIT 2180
+#define GsBIT 265
+#define ABIT 529
+#define AsBIT 1058
+#define BBIT 2120
+
+#endif
