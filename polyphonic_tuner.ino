3bd1d972390083a91bd0722225d5491ecec37119
Sam Kortchmar <Samuel_Kortchmar@brown.edu>
2015-11-21 14:40:22 -0500

    basic screen stencil

--- polyphonic_tuner.ino
+++ polyphonic_tuner.ino
@@ -1,9 +1,14 @@
+#include <LiquidCrystal.h>
+LiquidCrystal lcd(2, 4, 5, 6, 7, 8);
 void setup() {
+  lcd.begin(16, 2);
   // put your setup code here, to run once:
 
 }
 
 void loop() {
+  lcd.setCursor(1, 0);
+  lcd.print("HEY");
   // put your main code here, to run repeatedly:
 
 }
