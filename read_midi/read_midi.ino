87a45be67d1b61385818a33ec826c557e1affaf6
Sam Kortchmar <Samuel_Kortchmar@brown.edu>
2015-11-21 17:39:52 -0500

    working with library

--- read_midi/read_midi.ino
+++ read_midi/read_midi.ino
@@ -1,18 +1,36 @@
 #include <LiquidCrystal.h>
 LiquidCrystal lcd(2, 4, 5, 6, 7, 8);
-
+#include <MIDI.h>
+MIDI_CREATE_DEFAULT_INSTANCE();
 
 const int MIDI_NOTES = 127;
 boolean notes[MIDI_NOTES];
-byte noteOn = 144;
-byte noteOff = 128;
-byte currentNote = 0;
+#define NOTE_ON 144
+#define NOTE_OFF 128
+byte last_cmd = 0;
+byte last_note = 0;
 
-int noteOns = 0;
-
+// This function will be automatically called when a NoteOn is received.
+// It must be a void-returning function with the correct parameters,
+// see documentation here:
+// http://arduinomidilib.fortyseveneffects.com/a00022.html
+void handleNoteOn(byte channel, byte pitch, byte velocity) {
+  setNote(pitch, velocity > 0);
+}
+void handleNoteOff(byte channel, byte pitch, byte velocity)
+{
+  setNote(pitch, false);
+}
 
 void setup() {
-  Serial.begin(31250);
+  // Connect the handleNoteOn function to the library,
+  // so it is called upon reception of a NoteOn.
+  MIDI.setHandleNoteOn(handleNoteOn);  // Put only the name of the function
+  // Do the same for NoteOffs
+  MIDI.setHandleNoteOff(handleNoteOff);
+  // Initiate MIDI communications, listen to all channels
+  MIDI.begin(MIDI_CHANNEL_OMNI);
+  
   for(int i = 0; i < MIDI_NOTES; i++) {
     notes[i] = false;
   }
@@ -20,66 +38,43 @@ void setup() {
 
 void setNote(byte note, boolean status) {
   notes[note] = status;
+  if (status) {
+    last_note = note;  
+  }
 }
-
-enum Read_Mode {
-  STREAM,
-  NOTE_ON,
-  NOTE_OFF
-};
-
-Read_Mode rm = STREAM;
 
 void readStream() {
   if (Serial.available()) {
-    byte commandByte = Serial.read();
-    if (commandByte == noteOn) {
-      rm = NOTE_ON;
-    } else if (commandByte == noteOff) {
-      rm = NOTE_OFF;
-    } else {
-      rm = STREAM;
+    byte b = Serial.read();
+    if (isCommand(b)) {
+      last_cmd = b;
+      return;
+    } //else it's a data byte
+
+    while(!Serial.available()) {
+      //busy loop, sorry mom
     }
-  }  
+
+    byte v = Serial.read();
+    switch (last_cmd) {
+      case NOTE_ON:
+        setNote(b, v > 0);
+        break;
+      case NOTE_OFF:
+        setNote(b, false);
+        break;
+      default:
+        break;
+    }
+  } 
 }
 
-void readMIDI() {
-  switch(rm) {
-    case STREAM: return readStream();
-    case NOTE_ON: 
-      noteOns++;
-      return readNoteOn();
-    case NOTE_OFF: return readNoteOff();
-  }
+boolean isCommand(byte b) {
+  return b > 127;
 }
-
-void readNoteOn() {
-  if (Serial.available() < 2) {
-    return;
-  }
-
-  byte note = Serial.read();
-  byte velocity = Serial.read();
-  setNote(note, true);
-   
-  rm = STREAM;
-}
-
-void readNoteOff() {
-  if (Serial.available() < 2) {
-    return;
-  }
-
-  byte note = Serial.read();
-  byte velocity = Serial.read();
-  setNote(note, false);
-
-  rm = STREAM;
-}
-
 
 void loop() {
-  readMIDI(); 
-  lcd.setCursor(0, 1);
-  lcd.print(noteOns);
+  MIDI.read();
+  lcd.clear();
+  lcd.print(last_note);
 }
